1c29b883d62fff2f7c1f3d444a28a7edcc874b05
Showrove Roy <showrove200@gmail.com>
2022-11-10 18:04:26 +0600

    Env file added

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DBUSER_BUCKET}:${process.env.SECRET_KEY}@cluster0.in3ib7y.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Take Tour Travel server is Running");
