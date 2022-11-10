7d96089dae7bc939ffd2ac05bbe14eee9ceca3fd
Showrove Roy <showrove200@gmail.com>
2022-11-10 21:29:22 +0600

    single service api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -10,16 +10,42 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DBUSER_BUCKET}:${process.env.SECRET_KEY}@cluster0.in3ib7y.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const collections = client
+      .db("takeTourTravel")
+      .collection("travelServices");
+
+    // Get all service api
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = collections.find(query);
+
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get single service api
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+
+      const result = await collections.findOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Take Tour Travel server is Running");
